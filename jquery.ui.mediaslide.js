61eca5054d467dc5c3b44e607bd444933b5e13ff
Kieran Simkin <kieran@digital-crocus.com>
2011-12-26 19:57:29 +0000

    initial work on the data loader

--- jquery.ui.mediaslide.js
+++ jquery.ui.mediaslide.js
@@ -4,14 +4,61 @@
 $.widget( "ui.mediaslide", {
 	// These options will be used as defaults
 	options: { 
-		clear: null
+		"atom_xml_data": null,
+		"atom_xml_ajax": null,
+		"json_data": null,
+		"json_ajax": null
 	},
 
 	// Set up the widget
 	_create: function() {
+		this.setup = false;
+		this._init_data();
+	},
+	_init_data: function() { 
+		if (this.options.atom_xml_data != null) { 
+			this.dataType='atom';
+			this.data=jQuery.parseXML(this.options.atom_xml_data);
+			this._init_display();
+		} else if (this.options.atom_xml_ajax != null) { 
+			if (this.options.atom_xml_ajax.length!=null) { 
+				jQuery.ajax(this.options.atom_xml_ajax.url,this.options.atom_xml_ajax.options,function(data) { 
+					this.data=data;
+					this.dataType='atom';
+					this._init_display();
+				});
+			} else { 
+				jQuery.ajax(this.options.atom_xml_ajax,{},function(data) { 
+					this.data=data;
+					this.dataType='atom';
+					this._init_display();
+				});
+			}
+		} else if (this.options.json_data!= null) { 
+			this.data=jQuery.parseJSON(this.options.json_data);
+			this.dataType='json';
+			this._init_display();
+		} else if (this.options.json_ajax != null) { 
+			if (this.options.json_ajax.length!=null) { 
+				jQuery.getJSON(this.options.json_ajax.url,this.options.json_ajax.options,function(data) { 
+					this.data=data;
+					this.dataType='json';
+					this._init_display();
+				});
+			} else { 
+				jQuery.getJSON(this.options.json_ajax,{}, function(data) { 
+					this.data=data;
+					this.dataType='json';
+					this._init_display();
+				});
+			}
+		} else {
+			alert('No data specified.');
+		}
+	},
+	_init_display: function() { 
 
 	},
-
 	// Use the _setOption method to respond to changes to options
 	_setOption: function( key, value ) {
 		switch( key ) {
