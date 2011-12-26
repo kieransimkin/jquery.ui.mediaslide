1d52b06ed77357bd90a5edfa5377d0e24a2b16b6
Kieran Simkin <kieran@digital-crocus.com>
2011-12-26 21:32:57 +0000

    changes

--- jquery.ui.mediaslide.js
+++ jquery.ui.mediaslide.js
@@ -89,7 +89,7 @@ $.widget( "ui.mediaslide", {
 	_parse_data: function() { 
 		if (this.dataType=='atom') { 
 			this.data.find('entry').each(function(i,o) { 
-				alert(o);
+				alert(o.html());
 				//alert(o.html());
 			});
 		} else if (this.dataType=='json') { 
