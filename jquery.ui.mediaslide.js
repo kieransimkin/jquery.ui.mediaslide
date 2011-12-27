6dd66aa853e55dfd5952cb36671f539b9b1b3386
Kieran Simkin <kieran@digital-crocus.com>
2011-12-27 16:46:51 +0000

    changes

--- jquery.ui.mediaslide.js
+++ jquery.ui.mediaslide.js
@@ -160,7 +160,7 @@ $.widget( "ui.mediaslide", {
 		});
 		//append icon to handle
 		var handleHelper = scrollbar.find( ".ui-slider-handle" )
-		.css({'top': '.2em','height': '1.5em'})
+		.css({'top': '0px','height': '0.8em'})
 		.mousedown(function() {
 			scrollbar.width( handleHelper.width() );
 		})
