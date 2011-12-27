bb8a561df4566a0233639fc87a95adcc7e47f0da
Kieran Simkin <kieran@digital-crocus.com>
2011-12-27 17:22:37 +0000

    changes

--- jquery.ui.mediaslide.js
+++ jquery.ui.mediaslide.js
@@ -145,7 +145,7 @@ $.widget( "ui.mediaslide", {
 		var scrollPane = this.thumbslide, scrollContent = this.thumbslide_content;
 		
 		//build slider
-		this.scrollbar = this.thumbslide_slider.slider({min: '0', max: this.d.length,
+		this.scrollbar = this.thumbslide_slider.slider({step: 0.5,
 			slide: function( event, ui ) {
 				if ( scrollContent.width() > scrollPane.width() ) {
 					scrollContent.css( "margin-left", Math.round(
