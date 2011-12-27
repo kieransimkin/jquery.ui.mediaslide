6899f00d8052f294cd8c96d216d98cb660baa74d
Kieran Simkin <kieran@digital-crocus.com>
2011-12-27 12:34:16 +0000

    changes

--- jquery.ui.mediaslide.js
+++ jquery.ui.mediaslide.js
@@ -186,9 +186,11 @@ $.widget( "ui.mediaslide", {
 		var tob=this;
 		jQuery(active_frame).css({'z-index': 1});
 		jQuery(inactive_frame).css({'z-index': 2}).html('<img class="ui-widget-mediaslide-active-img">').find('.ui-widget-mediaslide-active-img').attr('src',this.d[this.position+offset].normal);
-		if (this.mainpicture.height()!=jQuery(inactive_frame).height() || this.mainpicture.width()!=jQuery(inactive_frame).width()) { 
-			jQuery(this.mainpicture).animate({height: jQuery(inactive_frame).height(), width: jQuery(inactive_frame).width()},'fast');
-		}
+		jQuery(inactive_frame).find('.ui-widget-mediaslide-active-img').bind("load", function() { 
+			if (tob.mainpicture.height()!=jQuery(inactive_frame).height() || tob.mainpicture.width()!=jQuery(inactive_frame).width()) { 
+				jQuery(tob.mainpicture).animate({height: jQuery(inactive_frame).height(), width: jQuery(inactive_frame).width()},'fast');
+			}
+		});
 		jQuery(inactive_frame).fadeTo('slow', 1.0, function() { 
 			tob._toggle_pframe();
 			jQuery(active_frame).css({opacity: 0}).hide();
@@ -212,6 +214,23 @@ $.widget( "ui.mediaslide", {
 	_setOption: function( key, value ) {
 		switch( key ) {
 			case "atom_xml_data":
+		if (tob.mainpicture.height()!=jQuery(inactive_frame).height() || tob.mainpicture.width()!=jQuery(inactive_frame).width()) { 
+			jQuery(tob.mainpicture).animate({height: jQuery(inactive_frame).height(), width: jQuery(inactive_frame).width()},'fast');
+		}
+		});
+		jQuery(inactive_frame).fadeTo('slow', 1.0, function() { 
+			tob._toggle_pframe();
+			jQuery(active_frame).css({opacity: 0}).hide();
+			tob.position=tob.position+offset;
+		});
+		//alert('slide: '+offset.toString());	
+	},
+	next: function() { 
+		this.position_slide(1);
+	},
+	previous: function() { 
+		this.position_slide(-1);
+	},
 			case "atom_xml_ajax":
 			case "json_data":
 			case "json_ajax":
