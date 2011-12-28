90cfb165bb4ede21b36be3e4444a3ea3ab2c43fc
Kieran Simkin <kieran@digital-crocus.com>
2011-12-28 13:47:07 +0000

    slide

--- jquery.ui.mediaslide.js
+++ jquery.ui.mediaslide.js
@@ -290,7 +290,7 @@ $.widget( "ui.mediaslide", {
 			return 100;
 		}
 		var p=pos-this._get_first_thumb_count();
-		return (p/((this.d.length-1)-this._get_last_thumb_count()))*100;
+		return (p/((this.d.length-1)-(this._get_last_thumb_count()+1)))*100;
 	},
 	_get_scroll_position_in_range: function(pcent) {
 
