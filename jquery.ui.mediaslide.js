9b45c998f6922d4f06be8f51519e7cf18f7ecd9e
Kieran Simkin <kieran@digital-crocus.com>
2011-12-28 00:15:18 +0000

    changes

--- jquery.ui.mediaslide.js
+++ jquery.ui.mediaslide.js
@@ -158,10 +158,9 @@ $.widget( "ui.mediaslide", {
 				if (me.preloadtimeout!=null) {
 					clearTimeout(me.preloadtimeout);
 				}
-				var zme=me;
-				me.preloadtimeout=setTimeout(function() { 
+				me.preloadtimeout=setTimeout(function(zme) { 
 					zme.do_thumbnail_image_loads(Math.floor(zme._get_scroll_position_estimate(ui.value)));
-				},500);
+				},500,me);
 			},
 			change: function(event, ui) {
 				me._do_thumbnail_image_loads(Math.floor(me._get_scroll_position_estimate(ui.value)));
