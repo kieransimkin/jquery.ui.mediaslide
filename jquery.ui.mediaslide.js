08b11975b206a5f3759eac370178e934f53df5f1
Kieran Simkin <kieran@digital-crocus.com>
2011-12-28 12:03:40 +0000

    add 2-clause BSD license

--- jquery.ui.mediaslide.js
+++ jquery.ui.mediaslide.js
@@ -1,4 +1,17 @@
-
+/*  jQuery.ui.mediaslide.js
+ *  by Kieran Simkin - http://SlinQ.com/
+ *
+ *  Copyright (c) 2011-2012, Kieran Simkin
+ *  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * 
+ *  -  Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *  -  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
 (function( $ ) {
 $.widget( "ui.mediaslide", {
@@ -227,7 +240,6 @@ $.widget( "ui.mediaslide", {
 		
 	},
 	_do_html_setup: function() { 
-		// setup element HTML here
 		this.element.html('');
 		this.mainpicture=jQuery('<div></div>')	.addClass('ui-widget')
 							.addClass('ui-widget-mediaslide-main-picture-div')
@@ -424,7 +436,6 @@ $.widget( "ui.mediaslide", {
 			});
 			tob._handle_thumb_slide(oldpos);
 		});
-		//alert('slide: '+offset.toString());	
 	},
 	position_slide_to: function(pos) { 
 		this.position_slide(pos-this.position);
