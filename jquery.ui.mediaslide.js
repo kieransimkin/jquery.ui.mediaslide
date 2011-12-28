c66fa65d175a60e7b78a18f8ae745b9224bfdbe7
Kieran Simkin <kieran@digital-crocus.com>
2011-12-28 01:27:33 +0000

    changes

--- jquery.ui.mediaslide.js
+++ jquery.ui.mediaslide.js
@@ -134,6 +134,10 @@ $.widget( "ui.mediaslide", {
 							.css({'float': 'left', 'position': 'relative', 'width': op.thumb_width, 'margin-left': me._get_left_thumb_spacing(),'margin-right': me._get_right_thumb_spacing(), 'text-align': 'center' })
 							.html('<img class="ui-widget-mediaslide-thumb-img">')
 							.appendTo(t);
+			p.find('.ui-widget-mediaslide-thumb-img').wrap('<a href="#" class="ui-widget-mediaslide-thumb-link" />');
+			p.find('.ui-widget-mediaslide-thumb-link').click(function() { 
+				console.log("clicked!");
+			});
 			l.push(p);
 
 		});
