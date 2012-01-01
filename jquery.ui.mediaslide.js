fe484ff787105d6cbb22fd95116573c827dd632e
Kieran Simkin <kieran@digital-crocus.com>
2012-01-01 20:15:30 +0000

    attempt to fix jitteryness on thumbnail load animation

--- jquery.ui.mediaslide.js
+++ jquery.ui.mediaslide.js
@@ -628,12 +628,12 @@ $.widget( "ui.mediaslide", {
 		for (var i=this._get_first_preload_thumb_position(pos);i<=this._get_last_preload_thumb_position(pos);i++) { 
 			if (l[i].find('.ui-widget-mediaslide-thumb-img').attr('src')!=d[i].thumb) { 
 				var tim=new Image();
-				console.log(i);
+				var ii=i;
 				jQuery(tim).bind("load",function() { 
 					l[i].find('.ui-widget-mediaslide-thumb-img:eq(0)').attr('src',d[i].thumb);
 					console.log(l[i].find('.ui-widget-mediaslide-thumb-img').parent().html());
 					console.log(d[i].thumb);
-					console.log(i);
+					console.log(ii);
 					l[i].find('.ui-widget-mediaslide-thumb-img').parent().parent().css({top: (0-jQuery(this).height())+'px',opacity: '0.0'});
 					l[i].find('.ui-widget-mediaslide-thumb-img').parent().parent().animate({top: '0px','opacity':'1.0'},'slow');
 				});
